f05c08d64f41fc35854fd22df506cc26bbfcd9c8
sulejmanbojadzic <bojadzicsulejman>
2023-01-26 15:49:24 +0100

    first commit

---
+++ hexagon.py
@@ -0,0 +1,27 @@
+#First row:
+a=int(input("Enter side lenght : "))
+space=" "*(a-1)
+stars="*"*a
+print(space+stars)
+#First half:
+space_copy=space
+innerspace=" "*(a-2)
+while True:
+    if space=="":
+        break
+    space=space.replace(" ","",1)
+    innerspace=innerspace+"  "
+    print(space+"*"+innerspace+"*")
+
+#Second half:
+while True:
+    if space == space_copy.replace(" ","",1):
+        break
+    space=space+" "
+    innerspace=innerspace.replace(" ","",2)
+    print(space+"*"+innerspace+"*")
+
+#Last row:
+    
+print(space_copy+stars)
+
---
+++ hexagonV2.py
@@ -0,0 +1,15 @@
+a=int(input("Unesite duzinu stranice : "))
+space=" "*(a-1)
+spacenum=len(space)
+unutrasnjirazmak=2
+stars="*"*a
+print(space+stars)
+for i in range(a-1):
+    spacenum-=1
+    unutrasnjirazmak+=2
+    print(spacenum*" "+"*"+unutrasnjirazmak*" "+ "*")
+for i in range(a-2):
+    spacenum+=1
+    unutrasnjirazmak-=2
+    print(spacenum*" "+"*"+unutrasnjirazmak*" "+ "*")
+print(space+stars)
