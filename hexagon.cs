fbab04428ed3f80128b3ae5a6afecab7e31e2513
Sulejman Bojadzic <109358821+sulejmanbojadzic@users.noreply.github.com>
2023-02-09 20:42:00 +0100

    Update hexagon.cs

--- hexagon.cs
+++ hexagon.cs
@@ -1,24 +1,9 @@
 ﻿using System;
 
 namespace OOP
-/*a=int(input("Enter the side lenght : "))
-space=" "*(a-1)
-spacenum=len(space)
-innerspace=a-2
-stars="*"*a
-print(space+stars)
-for i in range(a-1):
-    spacenum-=1
-    innerspace+=2
-    print(spacenum*" "+"*"+innerspace*" "+ "*")
-for i in range(a-2):
-    spacenum+=1
-    innerspace-=2
-    print(spacenum*" "+"*"+innerspace*" "+ "*")
-print(space+stars)
-*/
+
 {
-    class Program
+    class hexagon
     {
         static void Main(string[] args)
         {
